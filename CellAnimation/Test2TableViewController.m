5c20fbd2426bfade5c88408f0fd9459621af8a6c
warren <onecoderiscrazy@gmail.com>
2018-10-15 15:05:52 +0800

    分支的版本不能低于master 测试提交

--- CellAnimation/Test2TableViewController.m
+++ CellAnimation/Test2TableViewController.m
@@ -24,6 +24,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     ///master 分支下的改动，测试会不会在test1的分支下展示
+    //分支的基础版本不能低于master 
     
     ///test1 分支下的改动
     // Uncomment the following line to preserve selection between presentations.
